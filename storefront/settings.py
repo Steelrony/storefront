3bf487e7c9138e8afcd9d0c869f6fbed5f047be9
Uchechukwu Aronu <uchechukwuaronu@Uchechukwus-MacBook-Pro.local>
2022-02-08 00:36:26 -0500

    Add postgresql database connection and migrate

--- storefront/settings.py
+++ storefront/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -86,8 +87,12 @@ WSGI_APPLICATION = "storefront.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "storefront",
+        "USER": os.environ.get("NAME"),
+        "PASSWORD": os.environ.get("PASSWORD"),
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
